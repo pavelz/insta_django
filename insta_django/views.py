be3ff90c8f94eb2acd395c9910021de8bccb5764
Pavel Zaitsev <pavel@arslogi.ca>
2019-12-17 00:51:48 +0300

    image listing and upload works, so easy. wow.
    :wq

--- insta_django/views.py
+++ insta_django/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.core import serializers
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -11,9 +13,12 @@ def index(request):
 @csrf_exempt
 def photos(request):
     if request.method == 'POST':
+        photo = Photo(filename=request.POST['filename'], file=request.FILES['image'].file.read())
+        photo.save()
         return HttpResponse("OK 👾")
     else:
         data = Photo.objects.all()
-        return HttpResponse(serializers.serialize('json', data), content_type="application/json")
-
+        serialized = serializers.serialize('python', data, fields=['filename', 'file'])
+        photos = [d['fields'] for d in serialized]
+        return HttpResponse(json.dumps(photos), content_type="application/json")
 
